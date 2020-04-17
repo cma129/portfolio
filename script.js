300f41feb0e2fa203efb742afbba033d1c7386c4
cma129 <claudiamhahn@gmail.com>
2020-04-16 23:35:26 -0400

    first push

---
+++ script.js
@@ -0,0 +1,45 @@
+// For hover on iOS
+document.addEventListener("click", x=>0)
+
+// Nav bar styling change on scroll
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 10) {
+        $(".projects-header, .resume-header").addClass("active");
+    } else {
+        $(".projects-header, .resume-header").removeClass("active");
+    }
+});
+
+// Whole-page menu on mobile
+$(function() {
+	$('.hamburger').click(function() {
+		$('.menu').fadeIn('fast');
+	});
+
+	$('.exit-responsive').click(function() {
+		$('.menu').fadeOut('fast')
+	});
+});
+
+// Gradient background animation
+const granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ddc784', '#e4b32e'],
+                ['#e4b32e', '#ddc784']
+            ],
+            transitionSpeed: 2500
+        }
+    }
+});
+
+// Title type animation
+new TypeIt(".type", {
+    strings: `<h1>Hi, I'm <span>Claudia.</span></h1><h2>I'm a front-end web developer.</h2>`,
+    speed: 70,
+    waitUntilVisible: true
+}).go();
